31a092a61d6fb6725f17644c8703773b3dc8096b
Felipe Marques <felipe.comarques1208@gmail.com>
2023-07-28 15:42:04 -0300

    test: Integration test for GET method on tutors

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { app } from "./app";
+import { connectDB, closeDB } from "./database/db";
+
+let server: any;
+
+export const start = async (port: number = 3000) => {
+  try {
+    await connectDB(process.env.MONGO_URI!);
+    server = app.listen(port, () =>
+      console.log(`Server is listening port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const close = async () => {
+  if (server) {
+    closeDB;
+    server.close();
+    console.log("Server closed");
+  }
+};
