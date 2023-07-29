55fa62a193b5e8307920e61e769db8814822285a
Felipe Marques <felipe.comarques1208@gmail.com>
2023-07-29 15:39:08 -0300

    test: Integration test for DELETE method on pets

--- src/test/petDELETE.test.ts
+++ src/test/petDELETE.test.ts
@@ -9,7 +9,7 @@ describe("Test for DELETE method on Tutors", () => {
   let server: any;
 
   beforeAll((done) => {
-    server = start(5002);
+    server = start(5012);
     done();
   });
 
@@ -19,68 +19,71 @@ describe("Test for DELETE method on Tutors", () => {
   });
 
   beforeEach(async () => {
-    const response = await request(app)
+    const tutorResponse = await request(app)
       .post("/tutors")
       .set("Authorization", authToken)
       .send({
-        name: "Test tutor - DELETE",
+        name: "Test pet - DELETE",
         password: "12345",
         phone: "123456789",
-        email: "tutorDELETE@test.com",
+        email: "petDELETE@test.com",
         date_of_birth: "2000-12-12",
         zip_code: "12345",
       });
-    tutorId = response.body.new_tutor._id;
-    petId = null;
-  });
+    tutorId = tutorResponse.body.new_tutor._id;
 
-  afterEach(async () => {
-    await request(app)
-      .delete(`/tutor/${tutorId}`)
-      .set("Authorization", authToken);
-  });
-
-  it("Should delete a tutor with correct authentication and id", async () => {
-    const response = await request(app)
-      .delete(`/tutor/${tutorId}`)
-      .set("Authorization", authToken);
-    expect(response.status).toBe(204);
-  });
-
-  it("Should return 400 if tutor have a pet associeted", async () => {
-    const PetResponse = await request(app)
+    const petResponse = await request(app)
       .post(`/pet/${tutorId}`)
       .set("Authorization", authToken)
       .send({
-        name: "Pet 1",
+        name: "Test pet - DELTE",
         species: "dog",
         carry: "p",
         weight: 5,
         date_of_birth: "2000-01-01",
       });
-    petId = PetResponse.body.Pet._id;
-    const DeleteResponse = await request(app)
-      .delete(`/tutor/${tutorId}`)
-      .set("Authorization", authToken);
-    await request(app).delete(`/pet/${petId}/tutor/${tutorId}`) .set("Authorization", authToken);
-    expect(DeleteResponse.status).toBe(400);
-    expect(DeleteResponse.body).toEqual({
-        Msg: 'Error deleting: Unable to delete an existing owner with pets'
-    });
+    petId = petResponse.body.Pet._id;
   });
 
-  it("Should return 400 if it does have incorrect id", async () => {
+  afterEach(async () => {
+    await request(app)
+      .delete(`/pet/${petId}/tutor/${tutorId}`)
+      .set("Authorization", authToken);
+    await request(app)
+      .delete(`/tutor/${tutorId}`)
+      .set("Authorization", authToken);
+    petId = null;
+  });
+
+  it("Should delete a pet with correct authentication and id", async () => {
     const response = await request(app)
-      .delete(`/tutor/${tutorId + 1}`)
+      .delete(`/pet/${petId}/tutor/${tutorId}`)
+      .set("Authorization", authToken);
+    expect(response.status).toBe(204);
+  });
+
+  it("Should return 400 if it does have incorrect tutorId", async () => {
+    const response = await request(app)
+      .delete(`/pet/${petId}/tutor/${tutorId + 1}`)
       .set("Authorization", authToken);
     expect(response.status).toBe(400);
     expect(response.body).toEqual({
-        Msg: 'Error deleting: No tutor with informed id'
+      Msg: "Error deleting pet: No tutor with informed id",
+    });
+  });
+
+  it("Should return 400 if it does have incorrect petId", async () => {
+    const response = await request(app)
+      .delete(`/pet/${petId + 1}/tutor/${tutorId}`)
+      .set("Authorization", authToken);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      Msg: "Error deleting pet: The entered id does not belong to any pet",
     });
   });
 
   it("Should return 400 if it does not have authentication", async () => {
-    const response = await request(app).delete(`/tutor/${tutorId}`);
+    const response = await request(app).delete(`/pet/${petId}/tutor/${tutorId}`);
     expect(response.status).toBe(400);
     expect(response.body).toEqual({
         Msg: 'token not provided'
@@ -89,7 +92,7 @@ describe("Test for DELETE method on Tutors", () => {
 
   it("Should return 401 if it does have invalid authentication", async () => {
     const response = await request(app)
-      .delete(`/tutor/${tutorId}`)
+      .delete(`/pet/${petId}/tutor/${tutorId}`)
       .set("Authorization", `Bearer 123`);
     expect(response.status).toBe(401);
     expect(response.body).toEqual({
