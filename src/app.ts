2a7abb42f62dda805c5e2da34dbb5843421e2543
Felipe Marques <felipe.comarques1208@gmail.com>
2023-07-29 16:39:36 -0300

    test: Integration test for POST method on auth

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import router from "./Routes/routes";
-// import connectDB from "./database/db";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 import { start } from "./server";
@@ -9,20 +8,8 @@ import { start } from "./server";
 export const app = express();
 dotenv.config();
 
-// const port: number = 3000;
-
 app.use(express.json());
 app.use("/", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// const start = async () => {
-//   try {
-//     // connectDB
-//     await connectDB(process.env.MONGO_URI!);
-//     app.listen(port, () => console.log(`Server is listening port ${port}...`));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
 
 // start();
